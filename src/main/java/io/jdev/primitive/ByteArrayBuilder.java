0df02387c39ea16ad236b5de04a2ddf72b1aa95f
Gentjan Koliçaj <18643945+gentjankolicaj@users.noreply.github.com>
2024-11-14 15:44:17 +0100

    re added module.

---
+++ src/main/java/io/jdev/primitive/ByteArrayBuilder.java
@@ -0,0 +1,108 @@
+package io.jdev.primitive;
+
+
+import java.util.LinkedList;
+import java.util.List;
+import org.apache.commons.collections4.CollectionUtils;
+import org.apache.commons.lang3.ArrayUtils;
+
+public final class ByteArrayBuilder {
+
+  public static final byte[] ZERO_BYTES = new byte[0];
+  private final List<byte[]> bufferedArrays;
+
+  public ByteArrayBuilder() {
+    this(null);
+  }
+
+  public ByteArrayBuilder(byte[] initialBlock) {
+    this.bufferedArrays = new LinkedList<>();
+    if (ArrayUtils.isNotEmpty(initialBlock)) {
+      this.bufferedArrays.add(initialBlock);
+    }
+  }
+
+  public static byte[] toByteArray(List<byte[]> bufferedArrays) {
+    if (CollectionUtils.isEmpty(bufferedArrays)) {
+      return new byte[0];
+    } else {
+      int totalLength = bufferedArrays.stream().mapToInt(arr -> arr.length).sum();
+      if (totalLength == 0) {
+        return ZERO_BYTES;
+      } else {
+
+        //copy buffered blocks in final array
+        byte[] finalBlock = new byte[totalLength];
+        int offset = 0;
+
+        for (byte[] block : bufferedArrays) {
+          System.arraycopy(block, 0, finalBlock, offset, block.length);
+          offset += block.length;
+        }
+        return finalBlock;
+      }
+    }
+  }
+
+  public ByteArrayBuilder append(int b8) {
+    this.bufferedArrays.add(new byte[]{(byte) b8});
+    return this;
+  }
+
+  public ByteArrayBuilder appendOptimal(int i) {
+    this.bufferedArrays.add(IntUtils.getOptimalBytesBE(i));
+    return this;
+  }
+
+  public ByteArrayBuilder append(char c) {
+    this.bufferedArrays.add(CharUtils.getOptimalBytesBE(c));
+    return this;
+  }
+
+  public ByteArrayBuilder appendOptimal(char c) {
+    this.bufferedArrays.add(CharUtils.getOptimalBytesBE(c));
+    return this;
+  }
+
+  public ByteArrayBuilder appendTwoBytes(int b16) {
+    this.bufferedArrays.add(new byte[]{(byte) (b16 >> 8), (byte) b16});
+    return this;
+  }
+
+  public ByteArrayBuilder appendThreeBytes(int b24) {
+    this.bufferedArrays.add(new byte[]{(byte) (b24 >> 16), (byte) (b24 >> 8), (byte) b24});
+    return this;
+  }
+
+  public ByteArrayBuilder appendFourBytes(int b32) {
+    this.bufferedArrays.add(
+        new byte[]{(byte) (b32 >> 24), (byte) (b32 >> 16), (byte) (b32 >> 8), (byte) b32,});
+    return this;
+  }
+
+  public ByteArrayBuilder append(byte[] array) {
+    if (ArrayUtils.isNotEmpty(array)) {
+      this.bufferedArrays.add(array);
+    }
+    return this;
+  }
+
+  public byte[] toByteArray() {
+    int totalLength = bufferedArrays.stream().mapToInt(arr -> arr.length).sum();
+    if (totalLength == 0) {
+      return ZERO_BYTES;
+    } else {
+
+      //copy buffered blocks in final array
+      byte[] finalBlock = new byte[totalLength];
+      int offset = 0;
+
+      for (byte[] block : bufferedArrays) {
+        System.arraycopy(block, 0, finalBlock, offset, block.length);
+        offset += block.length;
+      }
+      return finalBlock;
+    }
+  }
+
+}
---
+++ src/test/java/io/jdev/primitive/ByteArrayBuilderTest.java
@@ -0,0 +1,92 @@
+package io.jdev.primitive;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.util.List;
+import org.junit.jupiter.api.Test;
+
+class ByteArrayBuilderTest {
+
+  @Test
+  void constructors() {
+    ByteArrayBuilder builder = new ByteArrayBuilder();
+    assertThat(builder.toByteArray()).hasSize(0);
+
+    ByteArrayBuilder builder2 = new ByteArrayBuilder().append(new byte[1]).append(new byte[0])
+        .append(new byte[2]);
+    assertThat(builder2.toByteArray()).hasSize(3);
+
+    ByteArrayBuilder builder3 = new ByteArrayBuilder(new byte[0]).append(new byte[1])
+        .append(new byte[0]).append(new byte[2]);
+    assertThat(builder3.toByteArray()).hasSize(3);
+
+    ByteArrayBuilder builder4 = new ByteArrayBuilder(new byte[1000]).append(new byte[1])
+        .append(new byte[0]).append(new byte[2]);
+    assertThat(builder4.toByteArray()).hasSize(1003);
+  }
+
+
+  @Test
+  void append() {
+    ByteArrayBuilder builder = new ByteArrayBuilder().append(1).append(1024).append('a');
+    assertThat(builder.toByteArray()).hasSize(3);
+
+    ByteArrayBuilder builder1 = new ByteArrayBuilder(new byte[100]).append(1).append(1024)
+        .append('a');
+    assertThat(builder1.toByteArray()).hasSize(103);
+  }
+
+
+  @Test
+  void appendTwoBytes() {
+    ByteArrayBuilder builder = new ByteArrayBuilder().append(1).appendTwoBytes(1024).append('a');
+    assertThat(builder.toByteArray()).hasSize(4);
+
+    ByteArrayBuilder builder1 = new ByteArrayBuilder(new byte[100]).append(1).appendTwoBytes(1024)
+        .append('a');
+    assertThat(builder1.toByteArray()).hasSize(104);
+  }
+
+  @Test
+  void appendThreeBytes() {
+    ByteArrayBuilder builder = new ByteArrayBuilder().append(1).appendThreeBytes(1024).append('a');
+    assertThat(builder.toByteArray()).hasSize(5);
+
+    ByteArrayBuilder builder1 = new ByteArrayBuilder(new byte[100]).append(1).appendThreeBytes(1024)
+        .append('a');
+    assertThat(builder1.toByteArray()).hasSize(105);
+  }
+
+  @Test
+  void appendFourBytes() {
+    ByteArrayBuilder builder = new ByteArrayBuilder().append(1).appendFourBytes(1024).append('a');
+    assertThat(builder.toByteArray()).hasSize(6);
+
+    ByteArrayBuilder builder1 = new ByteArrayBuilder(new byte[100]).append(1).appendFourBytes(1024)
+        .append('a');
+    assertThat(builder1.toByteArray()).hasSize(106);
+  }
+
+  @Test
+  void appendOptional() {
+    ByteArrayBuilder builder = new ByteArrayBuilder().appendOptimal(65536).appendFourBytes(1024)
+        .appendOptimal('a');
+    assertThat(builder.toByteArray()).hasSize(8);
+
+    ByteArrayBuilder builder1 = new ByteArrayBuilder(new byte[100]).appendOptimal(65536)
+        .appendFourBytes(1024).appendOptimal('a');
+    assertThat(builder1.toByteArray()).hasSize(108);
+  }
+
+  @Test
+  void toByteArray() {
+    List<byte[]> empty = List.of();
+    assertThat(ByteArrayBuilder.toByteArray(empty)).hasSize(0);
+
+    List<byte[]> list = List.of(new byte[]{1, 2, 3, 4, 5}, new byte[0], new byte[]{6},
+        new byte[]{7, 8, 9, 0}, new byte[]{'1', '2'});
+    assertThat(ByteArrayBuilder.toByteArray(list)).hasSize(12)
+        .containsExactly(new byte[]{1, 2, 3, 4, 5, 6, 7, 8, 9, 0, '1', '2'});
+  }
+
+}
